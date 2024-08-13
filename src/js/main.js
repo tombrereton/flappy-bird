24b2c17a747e89f752c6e782ba3b0e12a489bfe1
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-08-13 21:01:43 +1000

    add render method

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,21 @@
 class Game {
   constructor(canvas, context) {
     this.canvas = canvas;
-    this.context = context;
+    this.ctx = context;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+  }
+  render() {
+    this.ctx.fillRect(100, 100, 50, 150);
   }
 }
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  ctx.width = 720;
-  ctx.height = 720;
+  canvas.width = 720;
+  canvas.height = 720;
+
+  const game = new Game(canvas, ctx);
+  game.render();
 });
